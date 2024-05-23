ee3b6cec1715d134bc79c5374f1a1517d7f2de1c
TheDudeD6 <108148004+TheDudeD6@users.noreply.github.com>
2024-05-23 14:17:49 -0600

    Update key.py

--- key.py
+++ key.py
@@ -4,7 +4,7 @@ def xor_encrypt_decrypt(string, key):
         encrypted += chr(ord(string[i]) ^ key)
     return encrypted
 
-string = "aAhWVtB\\ui\\ui\\ufVPP]s|\\uT"
+string = "aAhWVtB\ui|Ud`"
 key = 31337
 
 encrypted_string = xor_encrypt_decrypt(string, key)
