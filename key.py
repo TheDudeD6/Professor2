44d041e7d462cf835f6ff19d1ac0fe6f4e6bfc30
TheDudeD6 <108148004+TheDudeD6@users.noreply.github.com>
2024-05-23 12:40:13 -0600

    Create key.py

---
+++ key.py
@@ -0,0 +1,11 @@
+def xor_encrypt_decrypt(string, key):
+    encrypted = ""
+    for i in range(len(string)):
+        encrypted += chr(ord(string[i]) ^ key)
+    return encrypted
+
+string = "aAhWVtB\\ui\\ui\\ufVPP]s|\\uT"
+key = 31337
+
+encrypted_string = xor_encrypt_decrypt(string, key)
+print("Encrypted String:", encrypted_string)
